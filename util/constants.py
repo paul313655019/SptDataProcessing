9019852f2de78c9667c570a88fc2ac6137bc3582
Amir Fathi <fathi0amir@gmail.com>
2025-03-25 18:00:39 +0800

    Adjusted the MSD_DIVISOR constant

--- util/constants.py
+++ util/constants.py
@@ -3,4 +3,4 @@ This file contains all the constants used in the project.
 """
 
 DT = 0.033  # Time interval between frames in seconds
-MSD_LENGTH_DIVISOR = 0.15  # The maximum lag time is set to 60% of the total time
+MSD_LENGTH_DIVISOR = 0.12  # The maximum lag time is set to 60% of the total time
